314e5dde8405e43d053cfeb0ce2039cb962eb89d
André Carvalho <andre.daniel.carvalho@gmail.com>
2021-03-13 13:23:39 +0100

    * Adds comment

--- src/main/kotlin/com/gildedrose/Item.kt
+++ src/main/kotlin/com/gildedrose/Item.kt
@@ -1,5 +1,9 @@
 package com.gildedrose
 
+/*
+* I would convert this to a data class and remove the toString, since thats impl on data classes
+* But the goblin wont allow me to touch this class :)
+*/
 open class Item(var name: String, var sellIn: Int, var quality: Int) {
     override fun toString(): String {
         return this.name + ", " + this.sellIn + ", " + this.quality
